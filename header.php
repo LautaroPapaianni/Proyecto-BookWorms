94a6ff08a9d840512cbac101833b04d7c831de87
Lautaro Papaianni <lapapaianni@uade.edu.ar>
2024-09-11 19:09:08 -0300

    first commit

---
+++ header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="stylesheet.css" rel="stylesheet" />
+    <title>Document</title>
+</head>
+
+<body>
+    <nav class="navbar">
+        <!-- Logo -->
+        <div class="logo">
+            <img src="BookWorms-logo.jpg" alt="Logo">
+        </div>
+        <!-- Navigation links -->
+        <ul class="nav-links">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+        <!-- Hamburger menu (for smaller screens) -->
+        <div class="hamburger">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+    </nav>
