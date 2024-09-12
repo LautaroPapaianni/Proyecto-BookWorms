17417a2228058e62d0840d0f96ebe87ebb01119e
Lautaro Papaianni <lapapaianni@uade.edu.ar>
2024-09-12 00:39:11 -0300

    primero

--- header.php
+++ header.php
@@ -2,29 +2,59 @@
 <html lang="en">
 
 <head>
-
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="stylesheet.css" rel="stylesheet" />
-    <title>Document</title>
+    <link href="stylesheet.css" rel="stylesheet">
+    <link rel="icon" href="/Aplicaciones Interactivas/Proyecto BookWorms/BookWorms-favicon.png" sizes="128x128"
+        type="image/x-icon">
+    <title>BookWorms | Main page</title>
 </head>
 
 <body>
-    <nav class="navbar">
-        <!-- Logo -->
-        <div class="logo">
-            <img src="BookWorms-logo.jpg" alt="Logo">
-        </div>
-        <!-- Navigation links -->
-        <ul class="nav-links">
-            <li><a href="#">Home</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Contact</a></li>
-        </ul>
-        <!-- Hamburger menu (for smaller screens) -->
-        <div class="hamburger">
-            <span></span>
-            <span></span>
-            <span></span>
-        </div>
-    </nav>
+    <header>
+        <nav class="navbar navbar-expand-md sticky-top">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">
+                    <img src="BookWorms-logo.png" alt="BookWorms" width="100" height="100">
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Library</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link disabled" aria-disabled="true">Social</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                                aria-expanded="false">
+                                You
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">Account settings</a></li>
+                                <li><a class="dropdown-item" href="#">Items saved</a></li>
+                                <li>
+                                    <hr class="dropdown-divider">
+                                </li>
+                                <li><a class="dropdown-item" href="#">Log out</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <form class="d-flex" role="search">
+                        <input class="form-control me-2" type="search" placeholder="Search a book, an autor or a genre" aria-label="Search">
+                        <button class="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+    </header>
--- index.php
+++ index.php
@@ -1,32 +1,6 @@
 <?php
 include "header.php";
 ?>
-    <div class="container" id="home">
-        <div class="row text-center ">
-             <div class="col-md-3 head-main ">             
-                            <i class="fa fa-bell color-blue "></i>
-                         <h3>  <a href="index.php">HOME</a></h3>
-   
-                 </div>
-            <div class="col-md-3 head-main">
-    
-                          <i class="fa fa-edit color-blue "></i>
-                         <h3>  <a href="about.php">ABOUT</a></h3>
-       
-                 </div>
-             <div class="col-md-3 head-main">
-
-                        <i class="fa fa-globe color-blue "></i>
-                         <h3>  <a href="free.php">FREE</a></h3>
-          
-                 </div>
-             <div class="col-md-3 head-main">
-                   <i class="fa fa-headphones color-blue"></i>
-                      <h3>  <a href="contacto.php">CONTACTO</a></h3>
-      
-                 </div>
-          </div>
-    </div>
 <?php
 include "footer.php";
 ?>
